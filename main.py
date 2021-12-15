2c2c60443d9e6949341b9426c4cf4bae49a6676a
Gael Ramos <esgaelramos@gmail.com>
2021-12-14 21:03:13 -0600

    Import from Pydantic

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 #Las anteriores no sé por qué aparecen
 from typing import Optional
 from enum import Enum
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import EmailStr
+from pydantic import PositiveFloat
+from pydantic import HttpUrl
